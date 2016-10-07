4261e19df34c558b2991bbfbecdddf74cfb2a80b
Franck Allimant <github@allimant.org>
2016-10-07 18:53:10 +0200

    2.3 compatibility, and général improvements

--- CmCIC.php
+++ CmCIC.php
@@ -25,22 +25,19 @@ namespace CmCIC;
 
 use CmCIC\Model\Config;
 use Propel\Runtime\Connection\ConnectionInterface;
+use Symfony\Component\Routing\Router;
 use Thelia\Core\HttpFoundation\Response;
 use Thelia\Core\Template\TemplateDefinition;
-use Thelia\Model\Base\Template;
 use Thelia\Model\ModuleImageQuery;
 use Thelia\Model\Order;
 use Thelia\Module\AbstractPaymentModule;
-use Thelia\Module\BaseModule;
-use Thelia\Module\PaymentModuleInterface;
 use Thelia\Tools\URL;
-use Symfony\Component\Routing\Router;
 
 class CmCIC extends AbstractPaymentModule
 {
     const DOMAIN_NAME = "cmcic";
     const JSON_CONFIG_PATH = "/Config/config.json";
-
+    
     const CMCIC_CTLHMAC = "V1.04.sha1.php--[CtlHmac%s%s]-%s";
     const CMCIC_CTLHMACSTR = "CtlHmac%s%s";
     const CMCIC_CGI2_RECEIPT = "version=2\ncdr=%s";
@@ -48,13 +45,12 @@ class CmCIC extends AbstractPaymentModule
     const CMCIC_CGI2_MACNOTOK = "1\n";
     const CMCIC_CGI2_FIELDS = "%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*";
     const CMCIC_CGI1_FIELDS = "%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s";
-    const CMCIC_URLPAIEMENT = "%s/%s";
-
-    protected $_sKey;
-    protected $_sUsableKey;
-
+    
+    protected $sKey;
+    protected $sUsableKey;
+    
     protected $config;
-
+    
     /**
      *
      * This method is call on Payment loop.
@@ -67,28 +63,29 @@ class CmCIC extends AbstractPaymentModule
     public function isValidPayment()
     {
         $debug = $this->getConfigValue('debug', false);
-
-        if ($debug == "0") {
-            return true;
+        
+        if ($debug) {
+            // Check allowed IPs when in test mode.
+            $testAllowedIps = $this->getConfigValue('allowed_ips', '');
+            
+            $raw_ips = explode("\n", $testAllowedIps);
+            
+            $allowed_client_ips = array();
+            
+            foreach ($raw_ips as $ip) {
+                $allowed_client_ips[] = trim($ip);
+            }
+            
+            $client_ip = $this->getRequest()->getClientIp();
+            
+            $valid = in_array($client_ip, $allowed_client_ips);
+        } else {
+            $valid = true;
         }
-
-        $testAllowedIps = $this->getConfigValue('allowed_ips', '');
-
-        $raw_ips = explode("\n", $testAllowedIps);
-
-        $allowed_client_ips = array();
-
-        foreach($raw_ips as $ip) {
-            $allowed_client_ips[] = trim($ip);
-        }
-
-        $client_ip = $this->getRequest()->getClientIp();
-
-        $valid = in_array($client_ip, $allowed_client_ips);
-
+        
         return $valid;
     }
-
+    
     public function postActivation(ConnectionInterface $con = null)
     {
         /* insert the images from image folder if first module activation */
@@ -96,52 +93,49 @@ class CmCIC extends AbstractPaymentModule
         if (ModuleImageQuery::create()->filterByModule($module)->count() == 0) {
             $this->deployImageFolder($module, sprintf('%s/images', __DIR__), $con);
         }
-
+        
         /* set module title */
         $this->setTitle(
             $module,
             array(
-                "en_US" => "CB",
-                "fr_FR" => "CB",
+                "en_US" => "Pay by Credit Card",
+                "fr_FR" => "Paiement par Carte Bancaire",
             )
         );
     }
-    /**
-     * @return mixed
-     */
+    
     public function pay(Order $order)
     {
         $c = Config::read(CmCIC::JSON_CONFIG_PATH);
         $currency = $order->getCurrency()->getCode();
-        $opts="";
+        $opts = "";
         $cmCicRouter = $this->container->get('router.cmcic');
         $mainRouter = $this->container->get('router.front');
-
+        
         $vars = array(
-            "url_bank"=> sprintf(self::CMCIC_URLPAIEMENT, $c["CMCIC_SERVER"], $c["CMCIC_PAGE"]),
-            "version"=>$c["CMCIC_VERSION"],
-            "TPE"=>$c["CMCIC_TPE"],
-            "date"=>date("d/m/Y:H:i:s"),
-            "montant"=>(string) round($order->getTotalAmount(), 2).$currency,
-            "reference"=>self::harmonise($order->getId(),'numeric',12),
-            "url_retour"=>URL::getInstance()->absoluteUrl($cmCicRouter->generate("cmcic.receive", array(), Router::ABSOLUTE_URL))."/".(string) $order->getId(),
-            "url_retour_ok"=>URL::getInstance()->absoluteUrl($mainRouter->generate("order.placed",array("order_id"=>(string) $order->getId()), Router::ABSOLUTE_URL)),
-            "url_retour_err"=>URL::getInstance()->absoluteUrl($cmCicRouter->generate("cmcic.payfail",array("order_id"=>(string) $order->getId()), Router::ABSOLUTE_URL)),
-            "lgue"=>strtoupper($this->getRequest()->getSession()->getLang()->getCode()),
-            "societe"=>$c["CMCIC_CODESOCIETE"],
-            "texte-libre"=>"0",
-            "mail"=>$this->getRequest()->getSession()->getCustomerUser()->getEmail(),
-            "nbrech"=>"",
-            "dateech1"=>"",
-            "montantech1"=>"",
-            "dateech2"=>"",
-            "montantech2"=>"",
-            "dateech3"=>"",
-            "montantech3"=>"",
-            "dateech4"=>"",
-            "montantech4"=>""
+            "version" => $c["CMCIC_VERSION"],
+            "TPE" => $c["CMCIC_TPE"],
+            "date" => date("d/m/Y:H:i:s"),
+            "montant" => (string)round($order->getTotalAmount(), 2) . $currency,
+            "reference" => $this->harmonise($order->getId(), 'numeric', 12),
+            "url_retour" => URL::getInstance()->absoluteUrl($cmCicRouter->generate("cmcic.receive", array(), Router::ABSOLUTE_URL)) . "/" . (string)$order->getId(),
+            "url_retour_ok" => URL::getInstance()->absoluteUrl($mainRouter->generate("order.placed", array("order_id" => (string)$order->getId()), Router::ABSOLUTE_URL)),
+            "url_retour_err" => URL::getInstance()->absoluteUrl($cmCicRouter->generate("cmcic.payfail", array("order_id" => (string)$order->getId()), Router::ABSOLUTE_URL)),
+            "lgue" => strtoupper($this->getRequest()->getSession()->getLang()->getCode()),
+            "societe" => $c["CMCIC_CODESOCIETE"],
+            "texte-libre" => "0",
+            "mail" => $this->getRequest()->getSession()->getCustomerUser()->getEmail(),
+            "nbrech" => "",
+            "dateech1" => "",
+            "montantech1" => "",
+            "dateech2" => "",
+            "montantech2" => "",
+            "dateech3" => "",
+            "montantech3" => "",
+            "dateech4" => "",
+            "montantech4" => ""
         );
-        $hashable=sprintf(
+        $hashable = sprintf(
             self::CMCIC_CGI1_FIELDS,
             $vars["TPE"],
             $vars["date"],
@@ -168,92 +162,62 @@ class CmCIC extends AbstractPaymentModule
             self::getUsableKey($c["CMCIC_KEY"])
         );
         $vars["MAC"] = $mac;
-
-        $parser = $this->container->get("thelia.parser");
-        $parser->setTemplateDefinition(
-            new TemplateDefinition(
-                'module_cmcic',
-                TemplateDefinition::FRONT_OFFICE
-            )
+        
+        return $this->generateGatewayFormResponse(
+            $order,
+            $c["CMCIC_SERVER"] . $c["CMCIC_PAGE"],
+            $vars
         );
-
-        $render = $parser->render("gotobankservice.html",$vars);
-
-        return Response::create($render);
     }
-
-    public static function harmonise($value, $type, $len)
+    
+    protected function harmonise($value, $type, $len)
     {
         switch ($type) {
             case 'numeric':
-                $value = (string) $value;
-                if(mb_strlen($value, 'utf8') > $len);
-                $value = substr($value, 0, $len);
+                $value = (string)$value;
+                if (mb_strlen($value, 'utf8') > $len) {
+                    $value = substr($value, 0, $len);
+                }
                 for ($i = mb_strlen($value, 'utf8'); $i < $len; $i++) {
                     $value = '0' . $value;
                 }
                 break;
             case 'alphanumeric':
-                $value = (string) $value;
-                if(mb_strlen($value, 'utf8') > $len);
-                $value = substr($value, 0, $len);
+                $value = (string)$value;
+                if (mb_strlen($value, 'utf8') > $len) {
+                    $value = substr($value, 0, $len);
+                }
                 for ($i = mb_strlen($value, 'utf8'); $i < $len; $i++) {
                     $value .= ' ';
                 }
                 break;
         }
-
+        
         return $value;
     }
-
+    
     public static function getUsableKey($key)
     {
-        $hexStrKey  = substr($key, 0, 38);
-        $hexFinal   = "" . substr($key, 38, 2) . "00";
-
-        $cca0=ord($hexFinal);
-
-        if ($cca0>70 && $cca0<97)
-            $hexStrKey .= chr($cca0-23) . substr($hexFinal, 1, 1);
-        else {
-            if (substr($hexFinal, 1, 1)=="M")
+        $hexStrKey = substr($key, 0, 38);
+        $hexFinal = "" . substr($key, 38, 2) . "00";
+        
+        $cca0 = ord($hexFinal);
+        
+        if ($cca0 > 70 && $cca0 < 97) {
+            $hexStrKey .= chr($cca0 - 23) . substr($hexFinal, 1, 1);
+        } else {
+            if (substr($hexFinal, 1, 1) == "M") {
                 $hexStrKey .= substr($hexFinal, 0, 1) . "0";
-            else
+            } else {
                 $hexStrKey .= substr($hexFinal, 0, 2);
+            }
         }
-
+        
         return pack("H*", $hexStrKey);
     }
-
+    
     public static function computeHmac($sData, $key)
     {
         return strtolower(hash_hmac("sha1", $sData, $key));
-    }
-
-    public static function HtmlEncode($data)
-    {
-        $SAFE_OUT_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890._-";
-        $result = "";
-        for ($i=0; $i<strlen($data); $i++) {
-            if (strchr($SAFE_OUT_CHARS, $data{$i})) {
-                $result .= $data{$i};
-            } elseif (($var = bin2hex(substr($data,$i,1))) <= "7F") {
-                $result .= "&#x" . $var . ";";
-            } else
-                $result .= $data{$i};
-
-        }
-
-        return $result;
-    }
-
-    public function getRequest()
-    {
-        return $this->container->get('request');
-    }
-
-    public function getCode()
-    {
-        return 'CmCIC';
     }
 }
--- Form/ConfigureCmCIC.php
+++ Form/ConfigureCmCIC.php
@@ -17,7 +17,7 @@
 /*      GNU General Public License for more details.                                 */
 /*                                                                                   */
 /*      You should have received a copy of the GNU General Public License            */
-/*	    along with this program. If not, see <http://www.gnu.org/licenses/>.     */
+/*	    along with this program. If not, see <http://www.gnu.org/licenses/>.         */
 /*                                                                                   */
 /*************************************************************************************/
 
@@ -25,8 +25,8 @@ namespace CmCIC\Form;
 
 use CmCIC\CmCIC;
 use Symfony\Component\Validator\Constraints\NotBlank;
-use Thelia\Form\BaseForm;
 use Thelia\Core\Translation\Translator;
+use Thelia\Form\BaseForm;
 
 class ConfigureCmCIC extends BaseForm
 {
@@ -34,13 +34,13 @@ class ConfigureCmCIC extends BaseForm
     {
         return "configurecmcic";
     }
-
+    
     protected function buildForm()
     {
         $values = null;
-        $path = __DIR__."/../".CmCIC::JSON_CONFIG_PATH;
+        $path = __DIR__ . "/../" . CmCIC::JSON_CONFIG_PATH;
         if (is_readable($path)) {
-            $values = json_decode(file_get_contents($path),true);
+            $values = json_decode(file_get_contents($path), true);
         }
         $this->formBuilder
             ->add('com_key', 'text', array(
@@ -48,7 +48,7 @@ class ConfigureCmCIC extends BaseForm
                 'label_attr' => array(
                     'for' => 'com_key'
                 ),
-                'data' => (null === $values ?'':$values["CMCIC_KEY"]),
+                'data' => (null === $values ? '' : $values["CMCIC_KEY"]),
                 'constraints' => array(
                     new NotBlank()
                 )
@@ -58,7 +58,7 @@ class ConfigureCmCIC extends BaseForm
                 'label_attr' => array(
                     'for' => 'TPE'
                 ),
-                'data' => (null === $values ?'':$values["CMCIC_TPE"]),
+                'data' => (null === $values ? '' : $values["CMCIC_TPE"]),
                 'constraints' => array(
                     new NotBlank()
                 )
@@ -68,7 +68,7 @@ class ConfigureCmCIC extends BaseForm
                 'label_attr' => array(
                     'for' => 'com_soc'
                 ),
-                'data' => (null === $values ?'':$values["CMCIC_CODESOCIETE"]),
+                'data' => (null === $values ? '' : $values["CMCIC_CODESOCIETE"]),
                 'constraints' => array(
                     new NotBlank()
                 )
@@ -82,41 +82,59 @@ class ConfigureCmCIC extends BaseForm
                     "mon" => "MONETICO"
                 ),
                 'required' => 'true',
-                'expanded'=>true,
-                'multiple'=>false,
+                'expanded' => true,
+                'multiple' => false,
                 'data' => (null === $values ?
-                            '':
-                            (preg_match("#cic-banques#i",$values["CMCIC_SERVER"])?
-                                "cic":
-                                (preg_match("#creditmutuel#i",$values["CMCIC_SERVER"])?
-                                    "cm":
-                                    (preg_match("#banque-obc#i",$values["CMCIC_SERVER"])?
-                                        "obc":
-                                        (preg_match("#monetico#i",$values["CMCIC_SERVER"])?
-                                            "mon":
-                                            ""
-                                        )
-                                    )
+                    '' :
+                    (preg_match("#cic-banques#i", $values["CMCIC_SERVER"]) ?
+                        "cic" :
+                        (preg_match("#creditmutuel#i", $values["CMCIC_SERVER"]) ?
+                            "cm" :
+                            (preg_match("#banque-obc#i", $values["CMCIC_SERVER"]) ?
+                                "obc" :
+                                (preg_match("#monetico#i", $values["CMCIC_SERVER"]) ?
+                                    "mon" :
+                                    ""
                                 )
                             )
-                          )
+                        )
+                    )
+                ),
+                'label_attr' => array(
+                    'help' => Translator::getInstance()->trans(
+                        "The module may be used with several banks. Please select your bank here.",
+                        [],
+                        CmCIC::DOMAIN_NAME
+                    )
+                ),
+
             ))
             ->add('page', 'text', array(
                 'label' => Translator::getInstance()->trans('page', [], CmCIC::DOMAIN_NAME),
                 'label_attr' => array(
-                    'for' => 'page'
+                    'help' => Translator::getInstance()->trans(
+                        "The page invoked on the payment server. The default should be OK in most cases.",
+                        [],
+                        CmCIC::DOMAIN_NAME
+                    )
                 ),
-                'data' => (null === $values ?'':$values["CMCIC_PAGE"]),
+                'data' => (null === $values ? '' : $values["CMCIC_PAGE"]),
                 'constraints' => array(
                     new NotBlank()
                 )
             ))
             ->add('debug', 'checkbox', array(
-                'label' => "Debug",
+                'label' => Translator::getInstance()->trans('Run in test mode', [], CmCIC::DOMAIN_NAME),
+                'required' => false,
                 'label_attr' => array(
-                    'for' => 'debug'
+                    'for' => 'debug',
+                    'help' => Translator::getInstance()->trans(
+                        "Check this box to test the payment system, using test credit cards to simulate various situations.",
+                        [],
+                        CmCIC::DOMAIN_NAME
+                    )
                 ),
-                'value' => (null !== $values && preg_match("#test#i", $values["CMCIC_SERVER"]) ?"true":""),
+                'data' => boolval(CmCIC::getConfigValue('debug', false))
             ))
             ->add(
                 'allowed_ips',
@@ -135,7 +153,6 @@ class ConfigureCmCIC extends BaseForm
                         'rows' => 3
                     )
                 )
-            )
-        ;
+            );
     }
 }
