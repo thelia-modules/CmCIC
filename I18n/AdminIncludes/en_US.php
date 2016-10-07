fa6d697ca1122d548859dbb870c2cb4d1c408da8
Franck Allimant <github@allimant.org>
2016-10-07 20:18:05 +0200

    Fixed missing translation

---
+++ I18n/AdminIncludes/en_US.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+    '<a href="%url">Click here</a> to download the communications log between the module and the bank server.' => '<a href="%url">Click here</a> to download the communications log between the module and the bank server.',
+    '<strong>URL de retour</strong>: %url' => '<strong>Callback URL</strong>: %url',
+    'Configuration CmCIC' => 'CmCIC Configuration',
+    'Please change the access rights' => 'The module configuration cannot be saved. Please check access rights of module\'s "Config" directory.',
+    'This is the value you should provide for the "URL de Retour" parameter in your bank back-office.' => 'This is the value you should provide for the "URL de Retour" parameter in your bank back-office.',
+    'Transaction log' => 'Transaction log',
+);
